c22607524e3f7cb8df3c0c2ae479a19d2ad6ea05
BeyondBitsLtda <beyondbitsltda@gmail.com>
2025-04-04 06:04:24 -0300

    Add files via upload

---
+++ scripts/demo/demo.sikuli/demo.py
@@ -0,0 +1,193 @@
+App.open(r"C:\Users\PC\Documents\repositorios\Beyond-AT\scripts\demo\Hello.txt")
+
+click("1743724711988.png")
+click("1743725291686.png")
+type("Ola! Eu sou o script de testes automatizados da Beyond Bits! Me chamo Beyond AT (Automatic Testing)")  # Simula a digitação de um texto
+type(Key.ENTER)
+type("Eu fui escrtito com uma biblioteca Java (Eca) kkkk")
+type(Key.ENTER)
+type("E funciono com uma mistura de Java e Python, chamada Jython, Sou o terror dos cordenadores de TI KKKK")
+type(Key.ENTER)
+type("Alem disso executo os comandos usando visao computacional.")
+type(Key.ENTER)
+type("Chega de conversa fiada e vamos a pratica! Irei fazer um teste em uma das versoes antigas do MG-GAME")
+type(Key.ENTER)
+type("Que Deus me ajude!")
+
+App.open(r"C:\Users\PC\Documents\MG---Game - antigo\Public\index.html")
+wait(2)
+
+click("1743719244283.png")
+wait(2)  # Espera 1 segundo antes de verificar novamente
+
+App.open(r"C:\Users\PC\Documents\repositorios\Beyond-AT\scripts\demo\plano.odt")
+
+wait(5)  # Espera 1 segundo antes de verificar novamente
+
+click("1743753126583.png")
+type("OK")
+
+click("1743752709329.png")
+
+wait(2) 
+
+click("1743719275768.png")
+
+type("Byte")  # Simula a digitação de um texto
+
+click("1743719416174.png")
+
+click("1743719275768.png")
+
+type("Bryan")  # Simula a digitação de um texto
+
+click("1743719416174.png")
+
+click("1743719275768.png")
+
+type("Beyond AT")  # Simula a digitação de um texto
+
+click("1743719416174.png")
+click("1743719275768.png")
+
+
+
+type("Galudao")  # Simula a digitação de um texto
+
+click("1743719416174.png")
+
+click("1743753362000.png")
+wait(5)
+click("1743753384998.png")
+type("ok")
+
+click("1743753442658.png")
+wait(2)
+
+click("1743719653420.png")
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+        
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+        
+
+click("1743720704294.png")
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+        
+
+click("1743720704294.png")
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+
+click("1743720704294.png")
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png") 
+wait(1)
+click("1743720704294.png")
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743720270692.png")
+
+click("1743720704294.png")
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743723586397.png")
+click("1743753469536.png")
+wait(5)
+click("1743753484864.png")
+type("ok")
+
+wait(2)
+click("1743754513851.png")
+
+App.open(r"C:\Users\PC\Documents\repositorios\Beyond-AT\scripts\demo\Goodbye.txt")
+wait(2)
+click("1743753969703.png")
+click("1743754318897.png")
+type("Isso foi apenas uma demo do tipo de teste funcional que sou capaz.")  # Simula a digitação de um texto
+type(Key.ENTER)
+type("Mas posso ser util em outros casos, se me parametrizarem bem, posso testar regras nao funcionais.")
+type(Key.ENTER)
+type("Posso ajudar voces a rodarem logs,e ate servir como uma especie de automacao.")
+type(Key.ENTER)
+type("Obrigado pela atencao de voces passo a palavra ao Brayan.")
+type(Key.ENTER)
+type("Beyond e o Futuro!")
