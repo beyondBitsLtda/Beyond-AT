4be4a8e977463fd73e7af54d8091de7218b1175c
BeyondBitsLtda <beyondbitsltda@gmail.com>
2025-04-06 09:40:58 -0300

    Update demo.py

--- scripts/demo/demo.sikuli/demo.py
+++ scripts/demo/demo.sikuli/demo.py
@@ -1,7 +1,7 @@
 App.open(r"C:\Users\PC\Documents\repositorios\Beyond-AT\scripts\demo\Hello.txt")
 
-click("1743724711988.png")
-click("1743725291686.png")
+click("1743938834587.png") #clica em um espaço em branco no txt
+click("1743938915013.png") #Clica no botao de expandir janela
 type("Ola! Eu sou o script de testes automatizados da Beyond Bits! Me chamo Beyond AT (Automatic Testing)")  # Simula a digitação de um texto
 type(Key.ENTER)
 type("Eu fui escrtito com uma biblioteca Java (Eca) kkkk")
@@ -16,172 +16,156 @@ type("Que Deus me ajude!")
 
 App.open(r"C:\Users\PC\Documents\MG---Game - antigo\Public\index.html")
 wait(2)
-
-click("1743719244283.png")
+click("1743939700274.png") # clique de segurança
+wait(1)
+click("1743939046849.png") # clica na demo do mg-game
 wait(2)  # Espera 1 segundo antes de verificar novamente
 
 App.open(r"C:\Users\PC\Documents\repositorios\Beyond-AT\scripts\demo\plano.odt")
 
 wait(5)  # Espera 1 segundo antes de verificar novamente
 
-click("1743753126583.png")
+click("1743939200980.png") # clica no botao azul do plano de teste
 type("OK")
 
-click("1743752709329.png")
+click("1743939262283.png") # miniminiza a tela do plano de teste
 
 wait(2) 
 
-click("1743719275768.png")
+click("1743939385247.png") # clica no campo de digitar nome do player
 
-type("Byte")  # Simula a digitação de um texto
+type("Jonas")  # Simula a digitação de um texto
 
-click("1743719416174.png")
+click("1743939869795.png") # clica em botão cadastrar
 
-click("1743719275768.png")
+click("1743939385247.png") # clica no campo de digitar nome do player
 
-type("Bryan")  # Simula a digitação de um texto
+type("Yuri")  # Simula a digitação de um texto
 
-click("1743719416174.png")
+click("1743939869795.png") # clica em botão cadastrar
 
-click("1743719275768.png")
+click("1743939385247.png") # clica no campo de digitar nome do player
 
-type("Beyond AT")  # Simula a digitação de um texto
+type("Lucas")  # Simula a digitação de um texto
 
-click("1743719416174.png")
-click("1743719275768.png")
+click("1743939869795.png") # clica em botão cadastrar
+click("1743939385247.png") # clica no campo de digitar nome do player
 
 
 
 type("Galudao")  # Simula a digitação de um texto
 
-click("1743719416174.png")
+click("1743939869795.png") # clica em botão cadastrar
 
-click("1743753362000.png")
+click("1743940054131.png")# clica no libere office na barra iniciar
 wait(5)
-click("1743753384998.png")
+click("1743940101394.png")# clica no quadrado vermelho no plano de teste
 type("ok")
 
-click("1743753442658.png")
+
+click("1743939262283.png") # miniminiza a tela do plano de teste
 wait(2)
 
-click("1743719653420.png")
+click("1743940164546.png") # clica em começar quiz
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
 
-
-click("1743720704294.png")
-
-wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
 
-
-click("1743720704294.png")
-
-wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
 
-
-click("1743720704294.png")
-
-wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
+
+click("1743940256504.png")# clica em proxima pergunta
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743940214390.png") # clica em verdadeiro 
+
+
+click("1743940256504.png")# clica em proxima pergunta
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743940214390.png") # clica em verdadeiro 
+
+click("1743940256504.png")# clica em proxima pergunta
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743940214390.png") # clica em verdadeiro 
         
 
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
 
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
         
-
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-        
-
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
+click("1743940214390.png") # clica em verdadeiro 
 
-
-click("1743720704294.png")
+click("1743940256504.png")# clica em proxima pergunta
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png") 
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 wait(1)
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
-
-wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
-
-wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
-
-wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743720270692.png")
-
-click("1743720704294.png")
+click("1743940214390.png") # clica em verdadeiro 
+click("1743940256504.png")# clica em proxima pergunta
 
 wait(1)  # Espera 1 segundo antes de verificar novamente
-click("1743723586397.png")
-click("1743753469536.png")
+click("1743941024348.png") # clica no ok da janela de fim de jogo
+
+wait(1)  # Espera 1 segundo antes de verificar novamente
+click("1743941131473.png") # clica no icone do plano de teste na barra de iniciar
 wait(5)
-click("1743753484864.png")
+click("1743941244835.png")# clica no quadrado verde no plano de teste
 type("ok")
-
 wait(2)
-click("1743754513851.png")
+click("1743941294913.png") # mioniminiza o arquivo do plano de teste
 
 App.open(r"C:\Users\PC\Documents\repositorios\Beyond-AT\scripts\demo\Goodbye.txt")
 wait(2)
-click("1743753969703.png")
-click("1743754318897.png")
+click("1743941334736.png") # maximiza a janela do arquivo txt
+click("1743941358685.png") # clica para iniicar o texto
 type("Isso foi apenas uma demo do tipo de teste funcional que sou capaz.")  # Simula a digitação de um texto
 type(Key.ENTER)
 type("Mas posso ser util em outros casos, se me parametrizarem bem, posso testar regras nao funcionais.")
